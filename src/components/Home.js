338be3c357ff8fdb33459d00218651731311438d
eskalona17 <eskalona_17@hotmail.com>
2019-09-26 20:39:26 +0200

    api works on console

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,15 +4,19 @@ import axios from 'axios';
 class Home extends Component {
 
     componentDidMount(){
-        // axios.get('https://pokeapi.co/api/v2/pokemon/ditto/')
-        axios.get('https://gateway.marvel.com:443/v1/public/characters?apikey=6e8f34cea393d16874d5c0168d855c9c')
+        axios.get(`https://gateway.marvel.com/v1/public/characters?limit=12&ts=1&apikey=6e8f34cea393d16874d5c0168d855c9c&hash=f68b97ab42350ebc7b61b55b5fd02e20`)
         .then(result=>{
             console.log(result)
         }).catch(console.log)
     }
     render(){
+
+        const { results } = this.props
         return(
+            <div>
             <h1>Hola paco</h1>
+            <h2>{results}</h2>
+            </div>
         )
     }
 }
