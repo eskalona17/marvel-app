fe1a6c2d8923438372631ea3d899800f2a948c58
eskalona17 <eskalona_17@hotmail.com>
2019-09-28 12:22:07 +0200

    names and images iterated on app

--- src/components/Character.js
+++ src/components/Character.js
@@ -1,26 +1,23 @@
-import React, { Component, Fragment } from 'react'
+import React, { Component, Fragment, Children } from 'react'
 
 class Character extends Component {
 
 
     render() {
 
-        const character = this.props
+        // const character = this.props
 
-        
-
-        console.log('hola soy props: '+this.props)
+        // console.log('hola soy props: '+this.props)
 
 
         return (
             <div className="Episode card">
-                {/* <img src={character.data.medium} className="card-img-top" alt="..." /> */}
+                
                 <div className="card-body">
-
-                    {/* {this.props.characters.map((character, i) =>(
-                        <h5 className="card-title">{character.name}</h5>
-                    ))} */}
-
+                    <ul>
+                        {this.props.character.name}
+                        <img src={`${this.props.character.thumbnail.path}.${this.props.character.thumbnail.extension}`} alt="images"/>
+                    </ul>
                 </div>
             </div>
         )
